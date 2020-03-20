553a3e336229355983c58f4980607f20f2ee1051
Paul Murdoch <paulgmurdoch@gmail.com>
2020-03-20 03:06:52 +0200

    Search Filter

--- app/Http/Controllers/WebsitesController.php
+++ app/Http/Controllers/WebsitesController.php
@@ -22,14 +22,32 @@ class WebsitesController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request)
     {
         // $websites = new Website($request->all());
 
-        $website = new Website;
+        $website = new Websites;
         $website->name = $request->input('name');
         $website->url = $request->input('url');
+        $website->save();
 
-        return response()->json($website->save());
+        return response()->json($website);
+    }
+
+    public function filter(Request $request) 
+    {
+        // Search by ID
+        if ($request->has('id')) {
+            return response()->json(Websites::where('id', $request->input('id'))->get());
+        }
+        // Search by Name
+        if ($request->has('name')) {
+            return response()->json(Websites::where('name', 'LIKE', "%{$request->input('name')}%")->get());
+        }
+        // Search by URL
+        if ($request->has('url')) {
+            return response()->json(Websites::where('url', 'LIKE', "%{$request->input('url')}%")->get());
+        }
+        return response()->json(Websites::all());
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('websites', 'WebsitesController@index');
+Route::post('websites', 'WebsitesController@create');
+// Search get Route
+Route::post('websites/search', 'WebsitesController@filter');
